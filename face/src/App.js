9ab0d9356115a9fdeaa76cde2fd2a381a4183402
lionmoroz <lion.vladislav96@gmail.com>
2020-02-26 13:04:44 +0200

    Connect aplication with api Clarifai

--- face/src/App.js
+++ face/src/App.js
@@ -1,43 +1,92 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Navigation from './components/Navigation';
 import Rank from './components/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
+import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
 import './App.css';
 
-const particleFunc = {
-		"particles": {
-			        "number": {
-			            "value": 100
-			        },
-			        "size": {
-			            "value": 3
-			        },
-			        "line_linked": {
-            				shadow: {
-            					enable: true,
-            					color: "#800080",
-            					blur: 5
-            				}
-            			}
-			    }
+const app = new Clarifai.App({
+ apiKey: '5cba64afbc2a4245b8032bca1989cff8'
+});
 
+const particleFunc = {
+	"particles": {
+		        "number": {
+		            "value": 100
+		        },
+		        "size": {
+		            "value": 3
+		        },
+		        "line_linked": {
+        				shadow: {
+        					enable: true,
+        					color: "#800080",
+        					blur: 5
+        				}
+        			}
+		   		}
 }
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-        params={particleFunc} />
-      <Navigation/>
-      <Logo/>
-      <Rank/>
-      <ImageLinkForm/>
-    </div>
-  );
+class App extends Component {
+	constructor(){
+		super();
+		this.state = {
+			input: '',
+			imageUrl: '',
+			box:{}
+		}
+	}
+
+	culculateFaceLocation = (data) =>{
+		const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+		const image = document.getElementById('inputimage');
+		const width = Number(image.width);
+		const height = Number(image.height);
+		return{
+			leftCol: clarifaiFace.left_col*width,
+			topRow: clarifaiFace.top_row*height,
+			rightCol: width - (clarifaiFace.right_col*width),
+			bottomRow: height - (clarifaiFace.bottom_row*height),
+		}
+	}
+
+	displayFaceBox = (box) =>{
+		console.log(box);
+		this.setState({box:box});
+	}
+
+	onInputChange = (event) =>{
+		this.setState({input:event.target.value});
+	}
+
+	onButtonSubmit = () =>{
+		this.setState({imageUrl:this.state.input});
+		app.models.predict(
+			Clarifai.FACE_DETECT_MODEL,
+		    // URL
+		    this.state.input
+		)
+		.then(response => this.displayFaceBox(this.culculateFaceLocation(response)))
+		.catch(err => console.log(err))// there was an error}
+
+	}
+	render(){
+		return (
+		    <div className="App">
+		      <Particles className='particles' params={particleFunc} />
+		      <Navigation/>
+		      <Logo/>
+		      <Rank/>
+		      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+		      <FaceRecognition box={this.state.box} imageUrl = {this.state.imageUrl}/>
+		    </div>
+		);
+	}
 }
 
 export default App;
--- face/src/components/ImageLinkForm/ImageLinkForm.js
+++ face/src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './ImageLinkForm.css';
 
 
-const ImageLinkForm = () =>{
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) =>{
 	return (
 		<div>
 			<p className='f3'>
@@ -12,8 +12,8 @@ const ImageLinkForm = () =>{
 			</p>
 			<div className='center'>
 				<div className="form center pa4 br-3 shadow-3">
-					<input className = 'f4 pa2 w-70 center' type='text'/>
-					<button className = 'w-30 grow f4 link pv2 ph3 dib white bg-purple br-dark'  > Detect </button>
+					<input className = 'f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+					<button className = 'w-30 grow f4 link pv2 ph3 dib white bg-purple br-dark' onClick = {onButtonSubmit} > Detect </button>
 				</div>
 			</div>
 		</div>
